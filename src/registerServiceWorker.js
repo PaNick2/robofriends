56ed51e1cf3e7f312ad4f8b60675562f0ac2ac5a
PaNick2 <73993335+PaNick2@users.noreply.github.com>
2021-09-08 15:18:55 +0300

    done

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -1,15 +1,8 @@
-<<<<<<< HEAD
 const isLocalhost = Boolean(
   window.location.hostname === 'localhost' ||
     
     window.location.hostname === '[::1]' ||
     
-=======
-
-const isLocalhost = Boolean(
-  window.location.hostname === 'localhost' ||
-    window.location.hostname === '[::1]' ||
->>>>>>> 94d90a3731ac6aff96c9233b2fdc86a2caadc9ae
     window.location.hostname.match(
       /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
     )
@@ -17,17 +10,12 @@ const isLocalhost = Boolean(
 
 export default function register() {
   if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
-<<<<<<< HEAD
     
     const publicUrl = new URL(process.env.PUBLIC_URL, window.location);
     if (publicUrl.origin !== window.location.origin) {
       
       
       
-=======
-    const publicUrl = new URL(process.env.PUBLIC_URL, window.location);
-    if (publicUrl.origin !== window.location.origin) {
->>>>>>> 94d90a3731ac6aff96c9233b2fdc86a2caadc9ae
       return;
     }
 
@@ -35,15 +23,10 @@ export default function register() {
       const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
 
       if (isLocalhost) {
-<<<<<<< HEAD
         
         checkValidServiceWorker(swUrl);
       } else {
         
-=======
-        checkValidServiceWorker(swUrl);
-      } else {
->>>>>>> 94d90a3731ac6aff96c9233b2fdc86a2caadc9ae
         registerValidSW(swUrl);
       }
     });
@@ -59,7 +42,6 @@ function registerValidSW(swUrl) {
         installingWorker.onstatechange = () => {
           if (installingWorker.state === 'installed') {
             if (navigator.serviceWorker.controller) {
-<<<<<<< HEAD
               
               
               
@@ -69,10 +51,6 @@ function registerValidSW(swUrl) {
               
               
               
-=======
-              console.log('New content is available; please refresh.');
-            } else {
->>>>>>> 94d90a3731ac6aff96c9233b2fdc86a2caadc9ae
               console.log('Content is cached for offline use.');
             }
           }
@@ -85,33 +63,22 @@ function registerValidSW(swUrl) {
 }
 
 function checkValidServiceWorker(swUrl) {
-<<<<<<< HEAD
   
   fetch(swUrl)
     .then(response => {
       
-=======
-  fetch(swUrl)
-    .then(response => {
->>>>>>> 94d90a3731ac6aff96c9233b2fdc86a2caadc9ae
       if (
         response.status === 404 ||
         response.headers.get('content-type').indexOf('javascript') === -1
       ) {
-<<<<<<< HEAD
         
-=======
->>>>>>> 94d90a3731ac6aff96c9233b2fdc86a2caadc9ae
         navigator.serviceWorker.ready.then(registration => {
           registration.unregister().then(() => {
             window.location.reload();
           });
         });
       } else {
-<<<<<<< HEAD
         
-=======
->>>>>>> 94d90a3731ac6aff96c9233b2fdc86a2caadc9ae
         registerValidSW(swUrl);
       }
     })
